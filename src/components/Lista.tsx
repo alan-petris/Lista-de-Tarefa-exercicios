017204559018763bafc926ad968dfb64e343abc1
Alan <alanpetris.dev@gmail.com>
2026-04-24 19:12:18 -0300

    Filtros adicionados

--- src/components/Lista.tsx
+++ src/components/Lista.tsx
@@ -18,7 +18,7 @@ export default function Lista() {
     );
     const [valorInput, setValorInput] = useState<string>("");
     const handleAdd = () => {
-        if (valorInput) {
+        if (valorInput.trim()) {
             setLista([
                 ...lista,
                 { id: Date.now(), text: valorInput.trim(), completed: false },
@@ -42,11 +42,66 @@ export default function Lista() {
         );
     };
 
+    const limparConcluidas = () => {
+        setLista((prev) => prev.filter((item) => !item.completed));
+    };
+
     const listaFiltrada = lista.filter((item) => {
         if (filtro === "concluidas") return item.completed;
         if (filtro === "pendentes") return !item.completed;
         return true; // todas
     });
+
+    const concluirTodas = () => {
+        setLista((prev) =>
+            prev.map((item) => {
+                if (!item.completed) {
+                    return { ...item, completed: !item.completed };
+                }
+                return item;
+            }),
+        );
+    };
+
+    const alternarTodas = () => {
+        setLista((prev) =>
+            prev.map((item) => {
+                return { ...item, completed: !item.completed };
+            }),
+        );
+    };
+
+    const resetarTodas = () => {
+        setLista((prev) =>
+            prev.map((item) => {
+                return { ...item, completed: false };
+            }),
+        );
+    };
+    const marcarDoisPrimeiros = () => {
+        setLista((prev) =>
+            prev.map((item, index) => {
+                if (index === 0 || index === 1) {
+                    return { ...item, completed: true };
+                }
+                return item;
+            }),
+        );
+    };
+
+    const editarTarefa = (id: number, text: string) => {
+        setLista((prev) =>
+            prev.map((item) => {
+                if (item.id === id) {
+                    return { ...item, text: "mudei" };
+                }
+                return item;
+            }),
+        );
+    };
+    const total = lista.length;
+    const concluidas = lista.filter((item) => item.completed).length;
+    const pendentes = total - concluidas;
     return (
         <>
             <h2>Lista de items</h2>
@@ -59,16 +114,26 @@ export default function Lista() {
                 onChange={(e) => setValorInput(e.target.value)}
             />
             <button onClick={handleAdd}>Add</button>
-            <button onClick={() => setFiltro("concluidas")}>
-                Concluídas {listaFiltrada.length}
-            </button>
-            <button onClick={() => setFiltro("pendentes")}>
-                Pendentes {listaFiltrada.length}
-            </button>
-            <button onClick={() => setFiltro("todas")}>
-                Todas {lista.length}
-            </button>
+
+            <div>
+                <p>Filtros disponíveis</p>
+                <button onClick={() => setFiltro("concluidas")}>
+                    Concluídas {concluidas}
+                </button>
+                <button onClick={() => setFiltro("pendentes")}>
+                    Pendentes {pendentes}
+                </button>
+                <button onClick={() => setFiltro("todas")}>
+                    Todas {total}
+                </button>
+            </div>
+
             <p>Filtro atual: {filtro}</p>
+            <button onClick={limparConcluidas}>Excluir completas</button>
+            <button onClick={concluirTodas}>Concluir todas tarefas</button>
+            <button onClick={alternarTodas}>Alternar Todas</button>
+            <button onClick={resetarTodas}>Resetar todas</button>
+            <button onClick={marcarDoisPrimeiros}>Marcar 2 primeiros</button>
             <div className="container-lista">
                 <ul>
                     <p>Tarefas: {listaFiltrada.length}</p>
@@ -88,6 +153,11 @@ export default function Lista() {
                             </button>
                             <button onClick={() => toggleConcluido(item.id)}>
                                 Concluir
+                            </button>
+                            <button
+                                onClick={() => editarTarefa(item.id, item.text)}
+                            >
+                                Editar
                             </button>
                         </li>
                     ))}
